ed408ccce1d70acc2729b29edd17a479235ae4ed
adl3416 <makara3416@gmail.com>
2022-02-18 06:00:45 +0100

    carrental

--- src/main/java/com/lecture/carrental/exception/AuthException.java
+++ src/main/java/com/lecture/carrental/exception/AuthException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED) // response
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class AuthException extends RuntimeException {
 
 
