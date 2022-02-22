271e69e158a7ea5d32a32d1d2752fcd561b2b62e
adl3416 <makara3416@gmail.com>
2022-02-22 22:30:12 +0100

    carrental

--- src/main/java/com/lecture/carrental/controller/UserController.java
+++ src/main/java/com/lecture/carrental/controller/UserController.java
@@ -1,19 +1,20 @@
 package com.lecture.carrental.controller;
 
 import com.lecture.carrental.domain.User;
+import com.lecture.carrental.dto.UserDTO;
 import com.lecture.carrental.security.jwt.JwtUtils;
 import com.lecture.carrental.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -25,10 +26,23 @@ import java.util.Map;
 @AllArgsConstructor
 @Produces(MediaType.APPLICATION_JSON)
 @RequestMapping()
+
+
 public class UserController {
     public UserService userService;
     public AuthenticationManager authenticationManager;
     public JwtUtils jwtUtils;
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('CUSTOMER') or hasRole('ADMIN')")
+    public ResponseEntity<UserDTO> getUserById(HttpServletRequest request){
+        Long id = (Long) request.getAttribute("id");
+        UserDTO user = userService.findById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+
+
     @PostMapping("/register")
     public ResponseEntity<Map<String, Boolean>> registerUser(@Valid @RequestBody User user) {
         userService.register(user);
@@ -36,6 +50,8 @@ public class UserController {
         map.put("User registered successfully!", true);
         return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
+
+
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> authenticateUser(@RequestBody Map<String, Object> userMap){
         String email = (String) userMap.get("email");
@@ -50,4 +66,36 @@ public class UserController {
         map.put("token", jwt);
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
+
+
+    @PutMapping("/user")
+    @PreAuthorize("hasRole('CUSTOMER') or hasRole('ADMIN')")
+    public ResponseEntity<Map<String, Boolean>> updateUser(HttpServletRequest request,
+                                                           @Valid @RequestBody UserDTO userDTO) {
+
+        Long id = (Long) request.getAttribute("id");
+        userService.updateUser(id, userDTO);
+
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("success", true);
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+    @PatchMapping("/user/auth")
+    @PreAuthorize("hasRole('CUSTOMER') or hasRole('ADMIN')")
+    public ResponseEntity<Map<String, Boolean>> updatePassword(HttpServletRequest request,
+                                                               @RequestBody Map<String, Object> userMap) {
+        Long id = (Long) request.getAttribute("id");
+        String newPassword = (String) userMap.get("newPassword");
+        String oldPassword = (String) userMap.get("oldPassword");
+
+        userService.updatePassword(id, newPassword, oldPassword);
+
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("success", true);
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/com/lecture/carrental/service/UserService.java
+++ src/main/java/com/lecture/carrental/service/UserService.java
@@ -1,52 +1,54 @@
 package com.lecture.carrental.service;
 
+
 import com.lecture.carrental.domain.Role;
 import com.lecture.carrental.domain.User;
 import com.lecture.carrental.domain.enumeration.UserRole;
+import com.lecture.carrental.dto.UserDTO;
 import com.lecture.carrental.exception.AuthException;
 import com.lecture.carrental.exception.BadRequestException;
 import com.lecture.carrental.exception.ConflictException;
 import com.lecture.carrental.exception.ResourceNotFoundException;
 import com.lecture.carrental.repository.RoleRepository;
 import com.lecture.carrental.repository.UserRepository;
+import com.lecture.carrental.security.service.UserDetailsServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
+
 @AllArgsConstructor
-@Service                                     // anatosyonla servise classi oldugunu bu classa soyluyoruz
+@Service
 public class UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
-
-    public void register (User user ) throws BadRequestException {
-
-
-
-        if(userRepository.existsByEmail(user.getEmail())){
-
-            throw new ConflictException(("Error: email is already in us!"));
+    private final static String USER_NOT_FOUND_MSG = "user with id %d not found";
+    public UserDTO findById(Long id) throws ResourceNotFoundException {
+        User user = userRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException(String.format(USER_NOT_FOUND_MSG, id)));
+        UserDTO userDTO = new UserDTO();
+        userDTO.setRoles(user.getRoles());
+        return new UserDTO(user.getFirstName(), user.getLastName(), user.getPhoneNumber(), user.getEmail(),
+                user.getAddress(), user.getZipCode(), user.getBuiltIn(), userDTO.getRoles());
+    }
+    public void register(User user) throws BadRequestException {
+        if (userRepository.existsByEmail(user.getEmail())){
+            throw new ConflictException("Error: Email is already in use!");
         }
-
-
         String encodedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
         Set<Role> roles = new HashSet<>();
         Role customerRole = roleRepository.findByName(UserRole.ROLE_CUSTOMER)
                 .orElseThrow(() -> new ResourceNotFoundException("Error: Role is not found."));
-
         roles.add(customerRole);
-
         user.setRoles(roles);
         userRepository.save(user);
     }
-
     public void login(String email, String password) throws AuthException {
         try {
             Optional<User> user = userRepository.findByEmail(email);
@@ -57,14 +59,35 @@ public class UserService {
         }
     }
 
+    public void updateUser(Long id, UserDTO userDTO) throws BadRequestException {
 
+        boolean emailExists = userRepository.existsByEmail(userDTO.getEmail());
+        Optional<User> userDetails = userRepository.findById(id);
 
+        if (userDetails.get().getBuiltIn()) {
+            throw new BadRequestException("You dont have permission to update user info!");
+        }
+
+        if (emailExists && !userDTO.getEmail().equals(userDetails.get().getEmail())){
+            throw new ConflictException("Error: Email is already in use!");
+        }
+
+        userRepository.update(id, userDTO.getFirstName(), userDTO.getLastName(), userDTO.getPhoneNumber(),
+                userDTO.getEmail(), userDTO.getAddress(), userDTO.getZipCode());
+    }
+
+    public void updatePassword(Long id, String newPassword, String oldPassword) throws BadRequestException {
+        Optional<User> user = userRepository.findById(id);
+        if (user.get().getBuiltIn()) {
+            throw new BadRequestException("You dont have permission to update password!");
+        }
+
+        if (!BCrypt.hashpw(oldPassword, user.get().getPassword()).equals(user.get().getPassword()))
+            throw new BadRequestException("password does not match");
+
+        String hashedPassword = passwordEncoder.encode(newPassword);
+        user.get().setPassword(hashedPassword);
+
+        userRepository.save(user.get());
+    }
 }
-
-
-
-
-
-
-
-
