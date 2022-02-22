109613c974320fb0f2916d1f71dc3eb7690c9dce
adl3416 <makara3416@gmail.com>
2022-02-22 22:30:12 +0100

    carrental

--- src/main/java/com/lecture/carrental/dto/UserDTO.java
+++ src/main/java/com/lecture/carrental/dto/UserDTO.java
@@ -1,12 +1,14 @@
-package com.lecture.carrental.domain;
+package com.lecture.carrental.dto;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.lecture.carrental.domain.Role;
 import com.lecture.carrental.domain.enumeration.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.*;
+import javax.persistence.Column;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -14,66 +16,52 @@ import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "users")
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class UserDTO {
 
     @Size(max = 15)
     @NotNull(message = "Please enter your first name")
-    @Column(nullable = false, length = 15)
     private String firstName;
-
     @Size(max = 15)
     @NotNull(message = "Please enter your last name")
-    @Column(nullable = false, length = 15)
     private String lastName;
 
-    @Size(min = 4, max = 60)
-    @NotNull(message = "Please enter your password")
-    @Column(nullable = false, length = 120)
-    private String password;
 
+    @JsonIgnore   // pasword artik gorunmeyecek
+    private String password;
     @Pattern(regexp = "^((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$",
             message = "Please enter valid phone number")
     @Size(min = 14, max = 14)
     @NotNull(message = "Please enter your phone number")
-    @Column(nullable = false, length = 14)
     private String phoneNumber;
-
     @Email(message = "Please enter valid email")
     @Size(min = 5, max = 150)
     @NotNull(message = "Please enter your email")
-    @Column(nullable = false, unique = true, length = 14)
     private String email;
-
     @Size(max = 250)
     @NotNull(message = "Please enter your address")
-    @Column(nullable = false, length = 250)
     private String address;
-
     @Size(max = 15)
     @NotNull(message = "Please enter your zip code")
-    @Column(nullable = false, length = 15)
     private String zipCode;
-
-    @Column(nullable = false)
-    private Boolean builtIn = false;
-
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
-
-    public Set<String> getRole() {
+    private Boolean builtIn;
+    private Set<String> roles;
+    public UserDTO(String firstName, String lastName, String phoneNumber, String email,
+                   String address, String zipCode, Boolean builtIn, Set<String> roles) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.builtIn = builtIn;
+        this.roles = roles;
+    }
+    public void setRoles(Set<Role> roles) {
         Set<String> roles1 = new HashSet<>();
         Role[] role = roles.toArray(new Role[roles.size()]);
         for (int i = 0; i < roles.size(); i++){
@@ -82,7 +70,6 @@ public class User {
             else
                 roles1.add("Customer");
         }
-        return roles1;
+        this.roles = roles1;
     }
-
 }
--- src/main/java/com/lecture/carrental/service/UserService.java
+++ src/main/java/com/lecture/carrental/service/UserService.java
@@ -1,12 +1,15 @@
 package com.lecture.carrental.service;
 
+
 import com.lecture.carrental.domain.Role;
 import com.lecture.carrental.domain.User;
 import com.lecture.carrental.domain.enumeration.UserRole;
+import com.lecture.carrental.dto.UserDTO;
 import com.lecture.carrental.exception.AuthException;
 import com.lecture.carrental.exception.BadRequestException;
 import com.lecture.carrental.exception.ConflictException;
 import com.lecture.carrental.exception.ResourceNotFoundException;
+import com.lecture.carrental.projection.ProjectUser;
 import com.lecture.carrental.repository.RoleRepository;
 import com.lecture.carrental.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -15,28 +18,43 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 @AllArgsConstructor
-@Service                                     // anatosyonla servise classi oldugunu bu classa soyluyoruz
+@Service
 public class UserService {
+
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final static String USER_NOT_FOUND_MSG = "user with id %d not found";
 
-    public void register (User user ) throws BadRequestException {
+    public List<ProjectUser> fetchAllUsers() {
+        return userRepository.findAllBy();
+    }
 
+    public UserDTO findById(Long id) throws ResourceNotFoundException {
+        User user = userRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException(String.format(USER_NOT_FOUND_MSG, id)));
 
+        UserDTO userDTO = new UserDTO();
+        userDTO.setRoles(user.getRoles());
 
-        if(userRepository.existsByEmail(user.getEmail())){
+        return new UserDTO(user.getFirstName(), user.getLastName(), user.getPhoneNumber(), user.getEmail(),
+                user.getAddress(), user.getZipCode(), user.getBuiltIn(), userDTO.getRoles());
+    }
 
-            throw new ConflictException(("Error: email is already in us!"));
+    public void register(User user) throws BadRequestException {
+
+        if (userRepository.existsByEmail(user.getEmail())){
+            throw new ConflictException("Error: Email is already in use!");
         }
-
 
         String encodedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
+
         Set<Role> roles = new HashSet<>();
         Role customerRole = roleRepository.findByName(UserRole.ROLE_CUSTOMER)
                 .orElseThrow(() -> new ResourceNotFoundException("Error: Role is not found."));
@@ -50,21 +68,49 @@ public class UserService {
     public void login(String email, String password) throws AuthException {
         try {
             Optional<User> user = userRepository.findByEmail(email);
+
             if (!BCrypt.checkpw(password, user.get().getPassword()))
                 throw new AuthException("invalid credentials");
+
         }catch (Exception e){
             throw new AuthException("invalid credentials");
         }
     }
 
+    public void updateUser(Long id, UserDTO userDTO) throws BadRequestException {
+
+        boolean emailExists = userRepository.existsByEmail(userDTO.getEmail());
+        Optional<User> userDetails = userRepository.findById(id);
+
+        if (userDetails.get().getBuiltIn()) {
+            throw new BadRequestException("You dont have permission to update user info!");
+        }
+
+        if (emailExists && !userDTO.getEmail().equals(userDetails.get().getEmail())){
+            throw new ConflictException("Error: Email is already in use!");
+        }
+
+        userRepository.update(id, userDTO.getFirstName(), userDTO.getLastName(), userDTO.getPhoneNumber(),
+                userDTO.getEmail(), userDTO.getAddress(), userDTO.getZipCode());
+    }
+
+
+    public void updatePassword(Long id, String newPassword, String oldPassword) throws BadRequestException {
+
+        Optional<User> user = userRepository.findById(id);
+
+        if (user.get().getBuiltIn()) {
+            throw new BadRequestException("You dont have permission to update password!");
+        }
+
+        if (!BCrypt.hashpw(oldPassword, user.get().getPassword()).equals(user.get().getPassword()))
+            throw new BadRequestException("password does not match");
+
+        String hashedPassword = passwordEncoder.encode(newPassword);
+        user.get().setPassword(hashedPassword);
+
+        userRepository.save(user.get());
+    }
 
 
 }
-
-
-
-
-
-
-
-
